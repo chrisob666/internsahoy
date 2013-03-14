81e4d1711fdb77d9d222574efb04b3f45a677626
Chris <chrisob666@gmail.com>
2013-03-13 21:57:37 -0500

    fixing qsort post method to txt

--- app/assets/javascripts/qsort.js
+++ app/assets/javascripts/qsort.js
@@ -7,25 +7,4 @@
  */
 // Sorting the list
 
-$(document).ready(function(){
-    $('#qsorts').sortable({
-        axis: 'y',
-        dropOnEmpty: false,
-        handle: '.handle',
-        cursor: 'crosshair',
-        items: 'li',
-        opacity: 0.4,
-        scroll: true,
-        update: function(){
-            $.ajax({
-                type: 'post',
-                beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-                data: $('#qsorts').sortable('serialize'),
-                dataType: 'script',
-                complete: function(request){
-                    $('#qsorts').effect('highlight');
-                },
-                url: '/qsorts/sort'})
-        }
-    });
-});
+
