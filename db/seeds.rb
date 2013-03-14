bc4e3c6b607e73fe0ba3da7449ee96917bf9555e
Chris <chrisob666@gmail.com>
2013-03-13 22:31:24 -0500

    added broscarello's email as an admin login, updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# These are entries to automatically add to the database with rake db:seed
+# For the attributes and groupings, the associated ID# depends on the order of their creation, starting at 1
 
-#add the qsort attributes
-s = ["Creative", "Problem Solver", "Good Listener", "Oral/Verbal Communication",
-     "Writing Skill", "Flexibility (work scheduling)",
-     "People Skills", "Punctual", "Accurate", "Efficient", "Multi-Tasker",
-     "Analytical", "Ability to Plan", "Skilled Researcher", "Reflective",
-     "Leadership", "Teamwork", "Stamina", "Fast paced worker"]
+# Define the admin accounts
+User.create(:email => 'admin@interns.com',
+            :user_type => 'admin',
+            :password => 'password',
+            :password_confirmation => 'password')
 
-s.each do |att|
+User.create(:email => 'TMuscare@cdm.depaul.edu',
+            :user_type => 'admin',
+            :password => 'password',
+            :password_confirmation => 'password')
+
+
+# List the attitudinal attributes
+attributes = ["Creative",
+     "Problem Solver",
+     "Good Listener",
+     "Oral/Verbal Communication",
+     "Writing Skill",
+     "Flexibility (work scheduling)",
+     "People Skills",
+     "Punctual",
+     "Accurate",
+     "Efficient",
+     "Multi-Tasker",
+     "Analytical",
+     "Ability to Plan",
+     "Skilled Researcher",
+     "Reflective",
+     "Leadership",
+     "Teamwork",
+     "Stamina",
+     "Fast paced worker"]
+
+
+# List the culture groupings.
+groupings = ["Grouping One",
+             "Grouping Two",
+             "Grouping Three",
+             "Grouping Four",
+             "Grouping Five",
+             "Grouping Six",
+             "Grouping Seven"]
+
+
+
+# Add the groupings and attributes to database
+attributes.each do |att|
   n = Qsort.create(name: att)
   n.position = n.id
   n.save
 end
 
-#create culture groupings (with the id # in this order)
-Groups.create(:description => "Grouping One")
-Groups.create(:description => "Grouping Two")
-Groups.create(:description => "Grouping Three")
-Groups.create(:description => "Grouping Four")
-Groups.create(:description => "Grouping Five")
-Groups.create(:description => "Grouping Six")
-Groups.create(:description => "Grouping Seven")
+groupings.each do |grp|
+  n = Groups.create(description: grp)
+end
 
-#create admin account
-User.create(:email => 'admin@asdf.com', :user_type => 'admin', :password => 'password', :password_confirmation => 'password')
